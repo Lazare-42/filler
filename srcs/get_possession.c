912497420f9d1eb385c10424d4fb4cad1bfe660a
Julien BOURSALY <jboursal@e3r10p10.42.fr>
2018-07-16 19:23:26 +0200

    maj

--- srcs/get_possession.c
+++ srcs/get_possession.c
@@ -1,24 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_test.c                                        :+:      :+:    :+:   */
+/*   get_possession.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jboursal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/16 18:48:11 by jboursal          #+#    #+#             */
-/*   Updated: 2018/07/16 19:13:10 by jboursal         ###   ########.fr       */
+/*   Created: 2018/07/16 19:21:22 by jboursal          #+#    #+#             */
+/*   Updated: 2018/07/16 19:23:20 by jboursal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/filler.h"
 
-int	main(void)
-{
-	t_sqrt	**board;
-	int		x_max;
-	int		y_max;
-
-	board = board_init(x_max, y_max);
-	print_board(x_max, y_max, board);
-	return (0);
-}
+void	()
