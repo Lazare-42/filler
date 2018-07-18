612d1cc6086af9099780897109046276343ef4ed
Julien BOURSALY <jboursal@e3r2p22.42.fr>
2018-07-18 02:20:51 +0200

    filler

--- srcs/MY_maintest.c
+++ srcs/MY_maintest.c
@@ -6,7 +6,7 @@
 /*   By: jboursal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 18:48:11 by jboursal          #+#    #+#             */
-/*   Updated: 2018/07/18 02:03:28 by jboursal         ###   ########.fr       */
+/*   Updated: 2018/07/18 02:12:07 by jboursal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdio.h>
 
 void		boundary_draw_old(t_sqrt **board, t_filler filler);
+t_sqrt	**board_random(t_filler game_settings);
 
 int	main(void)
 {
@@ -23,15 +24,15 @@ int	main(void)
 	float		score;
 
 	f.x_max = 20;
-	f.y_max = 1000;
-	board = board_malloc(f);
+	f.y_max = 20;
+	board = board_random(f);
 
-	board[1][1].possession = FDF_P1;
+	//board[1][1].possession = FDF_P1;
 	//board[1][2].possession = 0;
 	//board[1][3].possession = 0;
 	//board[0][3].possession = 0;
 	//board[0][4].possession = 0;
-	board[f.y_max - 2][f.x_max - 2].possession = FDF_P2;
+	//board[f.y_max - 2][f.x_max - 2].possession = FDF_P2;
 	//board[f.y_max - 3][f.x_max - 3].possession = 1;
 	//board[f.y_max - 4][f.x_max - 4].possession = 1;
 	//board[f.y_max - 5][f.x_max - 5].possession = 1;
--- srcs/board_init.c
+++ srcs/board_init.c
@@ -6,11 +6,46 @@
 /*   By: jboursal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 18:19:04 by jboursal          #+#    #+#             */
-/*   Updated: 2018/07/18 01:20:01 by lazrossi         ###   ########.fr       */
+/*   Updated: 2018/07/18 02:20:43 by jboursal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/filler.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //Ne pas oublier d'inclure le fichier time.h
+
+t_sqrt	**board_random(t_filler game_settings)
+{
+	int		y;
+	int		x;
+	t_sqrt	**board;
+
+	srand(time(0));
+	board = NULL;
+	if (!(board = (t_sqrt **)malloc(sizeof(t_sqrt *) * game_settings.y_max)))
+		return (0);
+	y = 0;
+	while (y < game_settings.y_max)
+	{
+		if (!(board[y] = (t_sqrt *)malloc(sizeof(t_sqrt) * game_settings.x_max)))
+			return (0);
+		x = 0;
+		while (x < game_settings.x_max)
+		{
+			board[y][x].boundary = 0;
+			if (random()%100 < 3)
+				board[y][x].possession = FDF_P1;
+			else if (random()%100 > 97)
+				board[y][x].possession = FDF_P2;
+			else
+				board[y][x].possession = 0;
+			x++;
+		}
+		y++;
+	}
+	return (board);
+}
 
 t_sqrt	**board_malloc(t_filler game_settings)
 {
@@ -64,7 +99,7 @@ t_sqrt **board_init(t_sqrt **board, t_filler game_settings)
 		{
 			if (buf[x + 4] != '.' && board[y][x].possession > 0
 			&& board[y][x].possession < 1)
-				board[y][x].possession = (buf[x + 4] = game_settings.opponent
+				board[y][x].possession = (buf[x + 4] == game_settings.opponent
 				|| buf[x + 4] == game_settings.opponent + 32);
 			x++;
 		}
