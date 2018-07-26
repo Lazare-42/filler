a0baa5c921740135653da4da10e9bdacc50151fd
Julien BOURSALY <jboursal@e3r13p12.42.fr>
2018-07-26 05:51:51 +0200

    algo

--- srcs/MY_maintest.c
+++ srcs/MY_maintest.c
@@ -6,7 +6,7 @@
 /*   By: jboursal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 18:48:11 by jboursal          #+#    #+#             */
-/*   Updated: 2018/07/26 02:06:29 by jboursal         ###   ########.fr       */
+/*   Updated: 2018/07/26 05:51:07 by jboursal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -143,7 +143,7 @@ int	main(void)
 	point.y = 6;
 
 	f.x_max = 20;
-	f.y_max = 70;
+	f.y_max = 500;
 	board = board_random(f, 0);
 	board_cpy = board_random(f, 0);
 
@@ -210,12 +210,12 @@ int	main(void)
 
 	print_board_old(f.x_max, f.y_max, board);
 	i = 0;
-	while (i++ < 2000)
+	while (i++ < 500)
 	  {
 		boundary_draw_new(board, f);
-		print_board_old(f.x_max, f.y_max, board);
+		//print_board_old(f.x_max, f.y_max, board);
 		//board_print(board, f);
-		getchar();
+		//getchar();
 		if (i % 2)
 		{
 			//pc = get_piece(pc);
@@ -228,7 +228,7 @@ int	main(void)
 			best_position = get_best_position_for_p2(board, &board_cpy, pc, f);
 			piece_write_for_p2(&board, pc, best_position);
 		}*/
-		printf("score = %.2f / %d\n", board_score_calc(board, f), f.x_max * f.y_max); fflush(stdout);
+		//printf("score = %.2f / %d\n", board_score_calc(board, f), f.x_max * f.y_max); fflush(stdout);
 	}
 	return (0);
 }
